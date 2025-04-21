2ef59f03ac361af5a6193beb03d47a8b0a5b84c6
criss <crissvevo@icloud.com>
2025-04-21 12:04:17 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -69,7 +69,7 @@ router.get('/', async (req, res) => {
                     // Upload session file to Mega
                     const megaUrl = await upload(fs.createReadStream(`${dirs}/creds.json`), `${generateRandomId()}.json`);
                     let stringSession = megaUrl.replace('https://mega.nz/file/', ''); // Extract session ID from URL
-                    stringSession = 'MALVIN-XMD~' + stringSession;  // Prepend your name to the session ID
+                    stringSession = 'CRISS-AI-' + stringSession;  // Prepend your name to the session ID
 
                     // Send the session ID to the target number
                     const userJid = jidNormalizedUser(num + '@s.whatsapp.net');
@@ -78,7 +78,7 @@ router.get('/', async (req, res) => {
                     // Send confirmation message
                     await MalvinTechInc.sendMessage(userJid, { text: `Hey there, MALVIN-XD User!* 👋🏻
 
-Thanks for using *MALVIN-XD* — your session has been successfully created!
+Thanks for using *CRISS-AI* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
@@ -87,15 +87,15 @@ Thanks for using *MALVIN-XD* — your session has been successfully created!
 
 *✅ Stay Updated:*  
 Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A
+https://whatsapp.com/channel/0029Vb0HIV2G3R3s2II4181g
 
 *💻 Source Code:*  
 Fork & explore the project on GitHub:  
-https://github.com/XdKing2/MALVIN-XD
+https://github.com/criss-vevo/CRISS-AI
 
 ——————
 
-*© Powered by Malvin King*
+*© Powered by CrissVevo*
 Stay cool and hack smart. ✌🏻` });
 
                     // Clean up session after use
